51240ef9933ec829638779e88941fce9d2f02c3f
gemtunde <gemtunde@gmail.com>
2024-01-15 10:19:34 +0100

    jest setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <h2>Nextjs Testing </h2>;
+  <div>
+    <h1>Home</h1>
+    <Link href="/about">About</Link>
+  </div>;
 }
